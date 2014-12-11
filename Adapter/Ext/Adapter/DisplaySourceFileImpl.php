b07efa43c9b5db9abe6a2a61a383b160ed74aba0
Hideyuki Shimooka <shimooka@doyouphp.jp>
2014-12-11 19:35:22 +0900

    php-cs-fixer

--- Adapter/Ext/Adapter/DisplaySourceFileImpl.php
+++ Adapter/Ext/Adapter/DisplaySourceFileImpl.php
@@ -4,7 +4,6 @@ namespace DoYouPhp\PhpDesignPattern\Adapter\Ext\Adapter;
 use DoYouPhp\PhpDesignPattern\Adapter\Ext\Target\DisplaySourceFile;
 use DoYouPhp\PhpDesignPattern\Adapter\ShowFile;
 
-
 /**
  * DisplaySourceFileを実装したクラス
  */
