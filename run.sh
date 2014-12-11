b81b0d69fea126f76d21fee3f677d4d6102c2a0d
Hideyuki Shimooka <shimooka@doyouphp.jp>
2014-12-11 20:32:17 +0900

    updated

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+php -v
+
 for pattern in AbstractFactory Adapter/Ext Adapter/Impl Bridge Builder ChainOfResponsibility Command Composite Decorator Facade FactoryMethod Flyweight Interpreter Iterator Mediator Memento Observer Prototype Proxy Singleton State Strategy TemplateMethod Visitor
 do
     echo $pattern
