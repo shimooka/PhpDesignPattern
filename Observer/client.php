b76eafda2ba5d783f28d7c5ff953130c3ac15e9f
Hideyuki Shimooka <shimooka@doyouphp.jp>
2014-12-25 14:03:54 +0900

    added comments

--- Observer/client.php
+++ Observer/client.php
@@ -3,19 +3,33 @@ namespace DoYouPhp\PhpDesignPattern\Observer;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-
-
+/**
+ * カートの作成
+ */
 $cart = new Cart();
+
+/**
+ * リスナーの登録
+ */
 $cart->addListener(new PresentListener());
 $cart->addListener(new LoggingListener());
 
+/**
+ * 商品を追加してみる
+ */
 $cart->addItem("Tシャツ");
 $cart->addItem("ぬいぐるみ");
 $cart->addItem("ぬいぐるみ");
 $cart->show();
 
+/**
+ * プレゼント対象の商品を追加してみる
+ */
 $cart->addItem("クッキーセット");
 $cart->show();
 
+/**
+ * プレゼント対象の商品を削除してみる
+ */
 $cart->removeItem("クッキーセット");
 $cart->show();
